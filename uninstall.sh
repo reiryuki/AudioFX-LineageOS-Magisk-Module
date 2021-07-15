3b9b2acbc67234c43f6bd69cf573f6655abbedd3
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2021-07-15 14:23:27 +0700

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,3 +0,0 @@
-rm -f `find /data/dalvik-cache -name *LineageAudioFX*`
-rm -f `find /data/system/package_cache -name *LineageAudioFX*`
-rm -f `find /data/system/package_cache -name *org.lineageos.audiofx*`
